bb03af1ff23783496463561873f31e2ae3d318aa
hidekiui <77121371+hidekiui@users.noreply.github.com>
2021-01-10 02:06:29 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,40 @@
+const hidekimenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do BOT :*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  FEITO POR *𝐻𝐼𝐷𝐸𝐾𝐼*
+  DUVIDAS? 👇
+  WA.me/5511946462131
+╚════════════════════`
+}
+
+exports.hidekimenu = hidekimenu
+
+
+
+
+
+
+
+
